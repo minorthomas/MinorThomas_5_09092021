bd39b3b1fffaed04fc0fd90ad307c3f7935cfd07
MINOR Thomas <minorthomas1@gmail.com>
2021-09-22 17:08:00 +0200

    Add lenses options

--- features/allproducts.js
+++ features/allproducts.js
@@ -3,57 +3,58 @@ let camerasUrl = 'http://localhost:3000/api/cameras'
 fetch(camerasUrl)
   .then((response) => response.json())
   .then((cameras) => {
-    console.log(cameras)
-  
-    //creer l'element section
-    let products = document.createElement("div")
-    products.setAttribute("id", "products")
-    products.classList.add("container")
-    document.querySelector("main").appendChild(products)
 
-    for (let camera of cameras) {
-      //creer et ajoute les elements article dans la section
-      let product = document.createElement("article")
-      product.classList.add("product")
-      products.appendChild(product)
+    function addCamerasInDom (camera){
 
-      let productLink = document.createElement("a")
-      let Url = camera._id      
-      productLink.setAttribute("href", `./pages/product.html?id=${Url}`)
-      productLink.classList.add("product_link")
-      product.appendChild(productLink)
-      
+      //creer l'element section
+      let products = document.createElement("div")
+      products.setAttribute("id", "products")
+      products.classList.add("container")
+      document.querySelector("main").appendChild(products)
 
-      //creer et ajoute les elements img dans chaque article
-      let productImage = document.createElement("img")
-      productImage.setAttribute("alt", "Appareil photo")
-      productImage.classList.add("product_img")
-      productLink.appendChild(productImage)
-      //ajoute l'image dans la balise img
-      productImage.src = camera.imageUrl
+    
+      for (let camera of cameras) {
 
-      //creer et ajoute les elements h2 dans chaque article
-      let productTitle = document.createElement("h2")
-      productTitle.classList.add("product_title")
-      productLink.appendChild(productTitle)
-      //ajoute le nom dans la balise h2
-      productTitle.textContent = camera.name
+        //creer et ajoute les elements article dans la section
+        let product = document.createElement("article")
+        product.classList.add("product")
+        products.appendChild(product)
 
-      //creer et ajoute les elements p et span dans chaque article
-      let productInfo= document.createElement("div")
-      productInfo.classList.add("product_info")
-      productLink.appendChild(productInfo)
+        let productLink = document.createElement("a")
+        let Url = camera._id      
+        productLink.setAttribute("href", `./pages/product.html?id=${Url}`)
+        productLink.classList.add("product_link")
+        product.appendChild(productLink)
+        
 
-      //creer et ajoute les elements p dans chaque article
-      let productDescription = document.createElement("p")
-      productInfo.appendChild(productDescription)
-      //ajoute la description dans la balise p
-      productDescription.textContent = camera.description
+        //creer et ajoute les elements img dans chaque article
+        let productImage = document.createElement("img")
+        productImage.setAttribute("alt", "Appareil photo")
+        productImage.classList.add("product_img")
+        productLink.appendChild(productImage)
+        //ajoute l'image dans la balise img
+        productImage.src = camera.imageUrl
 
-      //creer et ajoute les elements span dans chaque article
-      let productPrice = document.createElement("span")
-      productInfo.appendChild(productPrice)
-      //ajoute le prix dans la balise span
-      productPrice.textContent = camera.price + "€"
+        //creer et ajoute les elements p et span dans chaque article
+        let productInfo= document.createElement("div")
+        productInfo.classList.add("product_info")
+        productLink.appendChild(productInfo)
+
+        //creer et ajoute les elements h2 dans chaque article
+        let productTitle = document.createElement("h2")
+        productTitle.classList.add("product_title")
+        productInfo.appendChild(productTitle)
+        //ajoute le nom dans la balise h2
+        productTitle.textContent = camera.name
+
+        //creer et ajoute les elements span dans chaque article
+        let productPrice = document.createElement("span")
+        productInfo.appendChild(productPrice)
+        //ajoute le prix dans la balise span
+        productPrice.textContent = camera.price + "€"
+
+      }
     }
+
+    addCamerasInDom(cameras)
   }).catch((error) => console.log('Erreur: ' + error))
--- features/product.js
+++ features/product.js
@@ -8,63 +8,84 @@
 //---- bouton quantité de produit
 
 let searchUrlParams = window.location.search //Recupère la key id dans l'url
-let getUrlParams = new URLSearchParams(searchUrlParams) //Recupère les paramètres de l'url
+let getUrlParams = new URLSearchParams(searchUrlParams) 
 let getIdParam = getUrlParams.get("id") //Recupère l'id sans le ?
-
 
 let cameraUrl = `http://localhost:3000/api/cameras/${getIdParam}`
 
 fetch(cameraUrl)
   .then((response) => response.json())
   .then((camera) => {
-    console.log(camera)
-    
-    let products = document.createElement("div")
-    products.setAttribute("id", "products")
-    products.classList.add("container")
-    document.querySelector("main").appendChild(products)
 
-    let product = document.createElement("article")
-    product.classList.add("product")
-    products.appendChild(product)
+  addCameraInDom(camera)
 
-    let productLink = document.createElement("a")
-    let Url = camera._id      
-    productLink.setAttribute("href", `./pages/product.html?id=${Url}`)
-    productLink.classList.add("product_link")
-    product.appendChild(productLink)
-
-
-    //creer et ajoute les elements img dans chaque article
-    let productImage = document.createElement("img")
-    productImage.setAttribute("alt", "Appareil photo")
-    productImage.classList.add("product_img")
-    productLink.appendChild(productImage)
-    //ajoute l'image dans la balise img
-    productImage.src = camera.imageUrl
-
-    //creer et ajoute les elements h2 dans chaque article
-    let productTitle = document.createElement("h2")
-    productTitle.classList.add("product_title")
-    productLink.appendChild(productTitle)
-    //ajoute le nom dans la balise h2
-    productTitle.textContent = camera.name
-
-    //creer et ajoute les elements p et span dans chaque article
-    let productInfo= document.createElement("div")
-    productInfo.classList.add("product_info")
-    productLink.appendChild(productInfo)
-
-    //creer et ajoute les elements p dans chaque article
-    let productDescription = document.createElement("p")
-    productInfo.appendChild(productDescription)
-    //ajoute la description dans la balise p
-    productDescription.textContent = camera.description
-
-    //creer et ajoute les elements span dans chaque article
-    let productPrice = document.createElement("span")
-    productInfo.appendChild(productPrice)
-    //ajoute le prix dans la balise span
-    productPrice.textContent = camera.price + "€"
 })
+
+function addCameraInDom (camera) {
+  let products = document.createElement("div")
+  products.setAttribute("id", "products")
+  products.classList.add("container")
+  document.querySelector("main").appendChild(products)
+
+  let product = document.createElement("article")
+  product.classList.add("product")
+  products.appendChild(product)
+
+  // let productLink = document.createElement("a")
+  // let Url = camera._id      
+  // productLink.setAttribute("href", `./pages/product.html?id=${Url}`)
+  // productLink.classList.add("product_link")
+  // product.appendChild(productLink)
+
+  //creer et ajoute les elements img dans chaque article
+  let productImage = document.createElement("img")
+  productImage.setAttribute("alt", "Appareil photo")
+  productImage.classList.add("product_img")
+  product.appendChild(productImage)
+  //ajoute l'image dans la balise img
+  productImage.src = camera.imageUrl
+
+  //creer et ajoute les elements h2 dans chaque article
+  let productTitle = document.createElement("h2")
+  productTitle.classList.add("product_title")
+  product.appendChild(productTitle)
+  //ajoute le nom dans la balise h2
+  productTitle.textContent = camera.name
+
+  //creer et ajoute les elements p et span dans chaque article
+  let productInfo = document.createElement("div")
+  productInfo.classList.add("product_info")
+  product.appendChild(productInfo)
+
+  //creer et ajoute les elements p dans chaque article
+  let productDescription = document.createElement("p")
+  productInfo.appendChild(productDescription)
+  //ajoute la description dans la balise p
+  productDescription.textContent = camera.description
+
+  //creer et ajoute les elements span dans chaque article
+  let productPrice = document.createElement("span")
+  productInfo.appendChild(productPrice)
+  //ajoute le prix dans la balise span
+  productPrice.textContent = camera.price + "€"
+
+  //ajoute le formulaire pour choisir un objectif
+  let productFormLenses = document.createElement("form")
+  product.appendChild(productFormLenses)
+
+  let productLabelLenses = document.createElement("label")
+  productLabelLenses.setAttribute("for", "product_lenses")
+  productFormLenses.appendChild(productLabelLenses)
+
+  let productSelectLenses = document.createElement("select")
+  productSelectLenses.setAttribute("name", "product_lenses")
+  productFormLenses.appendChild(productSelectLenses)
+
+  for (let i = 0; i < (camera.lenses).length; i++) {
+    let productOptionsLenses = document.createElement("option")
+    productOptionsLenses.setAttribute("value", `${camera.lenses}`)
+    productSelectLenses.appendChild(productOptionsLenses)
+    productOptionsLenses.textContent = camera.lenses[i]
+  }
+}
 
